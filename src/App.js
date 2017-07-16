64f46714090b6a2823411ae44e25a6dbcd832273
kevin hu <hu.kevin.w@gmail.com>
2017-07-16 19:32:40 -0400

    Mount reddit api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import RedditPosts from './components/redditPosts'
 import './assets/styles/App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reddit_json: []
+    }
+  }
+
+  componentDidMount() {
+    var current = this;
+    var url = "https://www.reddit.com/r/mildlyinteresting/top/.json"
+
+    fetch(url)
+    .then(function(response) {
+      if (response.status >= 400) {
+        throw new Error("Bad response from server");
+      }
+      return response.json();
+    })
+    .then(function(resp) {
+      current.setState({ reddit_json: resp.data.children});
+    });
+  }
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <RedditPosts redditPosts={this.state.reddit_json}/>
+        </div>
       </div>
     );
   }
